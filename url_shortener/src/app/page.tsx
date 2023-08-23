ce0d62d5a2d3955f4e242faa1ff476ba3104c3b7
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-08-23 17:53:34 +0530

    refactor: add home page route

--- url_shortener/src/app/page.tsx
+++ url_shortener/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
-        <span className="text-red-500 font-inter text-2xl">Loading</span>
+        <button className="flex justify-center hover:bg-yellow-200 text-center items-center bg-yellow-400 hover:shadow-lg text-slate-900 m-2 px-4 py-2 font-medium shadow-md rounded-md  text-2xl">
+          <Link href="/home">Load Page </Link>
+        </button>
       </div>
     </main>
   );
